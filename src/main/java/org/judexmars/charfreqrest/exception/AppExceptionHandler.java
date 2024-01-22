19e98ae57860a9f6f1f4f2f254f9a295569f5784
JudexMars <danielpro2013rus@gmail.com>
2024-01-22 04:15:36 +0300

    feature: add server handling for invalid arguments

--- src/main/java/org/judexmars/charfreqrest/exception/AppExceptionHandler.java
+++ src/main/java/org/judexmars/charfreqrest/exception/AppExceptionHandler.java
@@ -1,10 +1,14 @@
 package org.judexmars.charfreqrest.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.*;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class AppExceptionHandler {
@@ -12,5 +16,15 @@ public class AppExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        List<String> body =
+                ex.getBindingResult().getAllErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .filter(Objects::nonNull)
+                        .toList();
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
